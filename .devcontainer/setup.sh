03010d69111eb5ec3feb75609d79c4c515a7a65e
roy <sn5diphone6s@gmail.com>
2024-04-08 18:00:14 +0800

    feat# first commit

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -4,4 +4,4 @@ curl https://sh.rustup.rs -sSf | sh -s -- -y
 # Update current shell environment variables after install to find rustup
 . "$HOME/.cargo/env"
 rustup install stable
-cargo install --force --path .
+bash install.sh
